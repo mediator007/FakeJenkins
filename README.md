ced2ee230a29d8fe3358ff54c9b65657878916f6
v.lyachin <v.lyachin@yadro.com>
2023-12-26 09:17:11 +0000

    server

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # FakeJenkins
+
+### Dev Launch
+
+```go run .```  
+
+Check server:  
+```curl localhost:8080/api/<id>/json```  
--- main.go
+++ main.go
@@ -1,71 +1,41 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
-	"log"
-
-	_ "github.com/mattn/go-sqlite3"
+	"net/http"
+	"strings"
 )
 
 func main() {
-	// Open the database, creating it if it doesn't exist
-	db, err := sql.Open("sqlite3", "db.sqlite3")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
+	http.HandleFunc("/api/", func(w http.ResponseWriter, r *http.Request) {
+		// Extract id from the URL path
+		id := extractID(r.URL.Path)
 
-	// Ping the database to check the connection
-	err = db.Ping()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("Connected to SQLite database")
-
-	// Create a table (if it doesn't exist)
-	createTableQuery := `
-		CREATE TABLE IF NOT EXISTS users (
-			id INTEGER PRIMARY KEY AUTOINCREMENT,
-			name TEXT NOT NULL,
-			age INTEGER
-		);
-	`
-	_, err = db.Exec(createTableQuery)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("Table 'users' created successfully")
-
-	// Insert data into the table
-	insertDataQuery := "INSERT INTO users (name, age) VALUES (?, ?);"
-	_, err = db.Exec(insertDataQuery, "John Doe", 30)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("Data inserted successfully")
-
-	// Query data from the table
-	rows, err := db.Query("SELECT id, name, age FROM users;")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer rows.Close()
-
-	fmt.Println("Query results:")
-	for rows.Next() {
-		var id, age int
-		var name string
-		err := rows.Scan(&id, &name, &age)
-		if err != nil {
-			log.Fatal(err)
+		// Check if id is empty
+		if id == "" {
+			http.Error(w, "Invalid URL pattern. Missing ID.", http.StatusBadRequest)
+			return
 		}
-		fmt.Printf("ID: %d, Name: %s, Age: %d\n", id, name, age)
+
+		// Respond with the extracted ID in JSON format
+		response := fmt.Sprintf(`{"id": "%s"}`, id)
+		w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte(response))
+	})
+
+	// Start the server on port 8080
+	fmt.Println("Server is running on :8080")
+	http.ListenAndServe(":8080", nil)
+}
+
+func extractID(path string) string {
+	// Split the path using "/"
+	parts := strings.Split(path, "/")
+
+	// Check if there are enough parts and the second part is "api"
+	if len(parts) >= 3 && parts[1] == "api" {
+		return parts[2] // The third part is the ID
 	}
 
-	// Handle errors from iterating over rows (if any)
-	if err := rows.Err(); err != nil {
-		log.Fatal(err)
-	}
+	return ""
 }
